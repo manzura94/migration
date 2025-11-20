b98be75f8394abc1804894c9df7083fd85af6429
manzura94 <manzura_jabborova@mail.ru>
2025-11-20 18:31:10 +0500

    feat: test husky install

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Download from "./components/Download";
 import Homepage from "./components/HomePage";
 
 export default function Home() {
-
+const test = 123;
   return (
     <main className="mainpage">
       <Homepage />
